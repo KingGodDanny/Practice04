0c0753911de66db2dbb0169f5dcc28de6904e71e
Danny <ctct25@naver.com>
2021-06-03 00:01:26 +0900

    21/05/31 배열과제 (해결못함...)

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,21 +4,28 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 		
-		int p=0;
+		
 		
 		int[] num = new int[6];
 		
 		for(int i=0; i<num.length; i++) {
-			num[i] = (int)(Math.random()*45)+1; 
-			
-				 for(int x=0; x<i; x++) {
-					if(num[i]!=num[i]) {
-						p = num[i];
+			num[i] = (int)(Math.random()*45)+1;  
+					
+					if(i>0 && num[i]==num[i+1]) {
+						num[i] = (int)(Math.random()*45)+1;
+						} 							
+							System.out.print(num[i] + "	");
+						
 					}
 					
-				 }
-				 System.out.print(num[i] + "	");
-			}
+						
+		
+							
+							
+			
+						 
+					
+				
 			
 					
 		
